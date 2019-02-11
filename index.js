016cf54cc8dcb0afe5d0f8724b3732d415b44a4b
reperez120 <46304321+reperez120@users.noreply.github.com>
2019-02-10 20:03:00 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function catCarousel(){
+  $('.thumbnail').on('click', function(event) {
+    const imgSrc = $(this).find('img').attr('src');
+    const imgAlt = $(this).find('img').attr('alt');
+     $('.hero img').attr('src', imgSrc).attr('alt', imgAlt);
+ 
+  });
+}
+
+$(catCarousel);  
